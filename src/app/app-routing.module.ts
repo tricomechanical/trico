76c91eb7f4c96350665b15d5ee3ac512616b144b
Don Kerrigan <donkerrigan@gmail.com>
2019-12-20 08:23:02 -0600

    Created base specialties page component and deleted duplicate directories in file paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { ContactComponent } from './pages/contact/contact/contact.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
-import { ContactComponent } from './pages/contact/contact/contact.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { SpecialtiesComponent } from './pages/specialties/specialties.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavigationBarComponent,
-    ContactComponent
+    ContactComponent,
+    SpecialtiesComponent
   ],
   imports: [
     BrowserModule,
